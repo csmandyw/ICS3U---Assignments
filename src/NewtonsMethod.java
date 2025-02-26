ea5df3de088fcd6d3c9c6d166f32302f21fadecf
unknown <342861705@TDSB.ON.CA>
2025-02-26 11:58:31 -0500

    created file

---
+++ src/NewtonsMethod.java
@@ -0,0 +1,12 @@
+// Newton's Method - Assignment 4 (Mar.19.2025) 
+import java.util.Scanner;
+
+public class NewtonsMethod {
+    public static void main(String[] args) throws Exception {
+        Scanner keyboard =  new Scanner(System.in);
+
+        System.out.println("Please enter your initial root guess");
+        double rootGuess = keyboard.nextDouble();
+        keyboard.close();
+    }
+}
