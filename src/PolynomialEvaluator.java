9c00afc8b89888bd4b9744e71e23e2181e876d70
unknown <342861705@TDSB.ON.CA>
2025-03-19 11:58:07 -0400

    failing

---
+++ src/PolynomialEvaluator.java
@@ -0,0 +1,12 @@
+// Polynomial Evaluator - Assignment 5 (Mar.21.2025)
+import java.util.Scanner;
+
+public class PolynomicalEvaluator {
+    public static void main(String[] args) throws Exception {
+        Scanner keyboard = new Scanner(System.in);
+        String function = "f(x)";
+        
+        System.out.println("Please enter your coefficients.");
+        System.out.println("Please enter your exponents.");
+    }
+}
