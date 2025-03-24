972891528a0182cd80d99e3d9c6be32aa6c6a429
Weng <Mandy.Weng@student.tdsb.on.ca>
2025-03-24 11:31:54 -0400

    got part of it fixed??

--- src/PowerRule.java
+++ src/PowerRule.java
@@ -3,17 +3,44 @@ import java.util.Scanner;
 
 public class PowerRule {
     public static void main(String[] args) throws Exception {
-        Scanner keyboard = new Scanner (System.in);
+        Scanner keyboard = new Scanner(System.in);
+        String firstDerivative = "f'(x) =";
+        String secondDerivative = "f''(x) =";
 
-        System.out.println("Please enter your coefficients, seperated by a space.");
+        System.out.println("Please enter your coefficients, separated by a space.");
         String coefficients = keyboard.nextLine();
-        System.out.println("Please enter your exponents, seperated by a space.");
+        System.out.println("Please enter your exponents, separated by a space.");
         String exponents = keyboard.nextLine();
         keyboard.close();
 
         String[] coeffs = coefficients.split(" ");
         String[] expos = exponents.split(" ");
-        
-        
+
+        for (int i = 0; i < coeffs.length; i++) {
+            double coeff = Double.parseDouble(coeffs[i]);
+            int exp = Integer.parseInt(expos[i]);
+
+            // first derivative
+            if (coeff != 0) {
+                double firstCoeff = coeff * exp;
+                int firstExpo = exp - 1;
+                if (firstExpo >= 0) {
+                    firstDerivative += (firstCoeff > 0 ? "+" : "") + firstCoeff + "x^" + firstExpo + " ";
+                }
+            }
+            // second derivative
+            if (coeff != 0) {
+                double secondCoeff = coeff * exp * (exp - 1);
+                int secondExpo = exp - 2;
+                if (secondExpo >= 0) {
+                    secondDerivative += (secondCoeff > 0 ? "+" : "") + secondCoeff + "x^" + secondExpo + " ";
+                }
+            }
+            if (coeff == (int)coeff) {
+                System.out.println((int)coeff);
+            }
+        }
+        System.out.println("Your first derivative is: " + firstDerivative);
+        System.out.println("Your second derivative is: " + secondDerivative);
     }
 }
